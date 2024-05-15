f595f0cf2885c4d0ded670e330440a4c24fb0b06
FlashCom Team <132670513+FlashComTeam@users.noreply.github.com>
2024-05-15 09:28:58 +0530

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?
+
+$conf = [
+	//Change to your bot token, check the readme to know how to create and get your bot token.
+	'bot_token' => '6330691394:AAHnB5VZX2LMI5iUd3TmqGrQ8fC0EUaNMZE',
+	//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
+	'only_trusted' => FALSE,
+	//Populate the array with the trusted Chat IDs
+	'trusted' => [ 
+		'<chat_id>' 
+		],
+	];
+
